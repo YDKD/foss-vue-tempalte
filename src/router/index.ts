d106a49cf0230d8ec25ccb5f3bf16a5ffb3beb22
YDKD <1606354057@qq.com>
2023-06-13 16:41:54 +0800

    feat(template): add default route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('@/views/Home/index.vue')
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+  routes,
+  history: createWebHistory()
 })
 
 export default router
